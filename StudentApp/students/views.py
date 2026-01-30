90869f9833a0cf6d6e936f5938dcfc0bcad67cdf
Adhwaith A M <131666137+adhwaith43@users.noreply.github.com>
2026-01-30 11:01:44 +0530

    Added neccassary import and comments in views.py to improve code clarity and functionality.

--- StudentApp/students/views.py
+++ StudentApp/students/views.py
@@ -13,7 +13,7 @@ from rest_framework import status
 
 #API VIEW reading all student records
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentlist(request):
     s=Student.objects.all()
     if (request.method == 'GET'):
@@ -22,7 +22,7 @@ def studentlist(request):
         return Response(stu.data,status=status.HTTP_200_OK) # sends the response back to Client side with status code 200
     
     if (request.method =='POST'):
-        s=StudentSerializer(data=request.data)
-        if s.is_valid():
-            s.save()
-            return Response(s.data,status=status.HTTP_201_CREATED)
+        s=StudentSerializer(data=request.data) # deserialize the json format data into django format using serializer class
+        if s.is_valid(): # checks whether data is valid or not
+            s.save() # if valid saves the data into table
+            return Response(s.data,status=status.HTTP_201_CREATED) # sends created record as response with status code 201
