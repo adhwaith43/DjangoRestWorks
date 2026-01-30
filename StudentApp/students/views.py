29d793b764dbdccdeec22e0396d5c41bc2deacf8
Adhwaith A M <131666137+adhwaith43@users.noreply.github.com>
2026-01-30 10:44:57 +0530

    Commit 2

--- StudentApp/students/views.py
+++ StudentApp/students/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# from rest_framework
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from students.models import Student
+
+
+
+
+#API VIEW reading all student records
+
+@api_view(['GET'])
+def studentlist(request):
+    s=Student.objects.all()
+    if (request.method == 'GET'):
+        s=Student.objects.all() #read all student records
+        stu=StudentSerializer(s,many=True)
+        return Response(stu.data,status=status.HTTP_200_OK)
