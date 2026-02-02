8e73c42d5d0c710f71a2fa90eb5593fc4cfafe3d
Adhwaith A M <131666137+adhwaith43@users.noreply.github.com>
2026-02-02 10:15:36 +0530

    corrected settingd,urls and views for student app

--- StudentAppclassbased/StudentAppclassbased/urls.py
+++ StudentAppclassbased/StudentAppclassbased/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from students import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('students/',views.studentlist),
+    path('studentdetail/<int:pk>',views.studentdetail),
 ]
--- StudentAppclassbased/students/views.py
+++ StudentAppclassbased/students/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from students.seraializers import StudentSerializer
+from students.models import Student
+from rest_framework import status
+from rest_framework.views import APIView
+class Studentlist(APIView):
+
+    def get(self,request):
+        s=Student.objects.all() #read all student records
+        stu=StudentSerializer(s,many=True) #serialize these records into json objects
+        return Response(stu.data,status=status.HTTP_200_OK) # sends the response back to Client side with status code 200
+        
+    def post(self,request):
+        s=StudentSerializer(data=request.data) # deserialize the json format data into django format using serializer class
+        if s.is_valid(): # checks whether data is valid or not
+            s.save() # if valid saves the data into table
+            return Response(s.data,status=status.HTTP_201_CREATED) # sends created record as response with status code 201
+        
