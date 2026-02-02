e4118f87934670f4bc9781dc906660764f413200
Adhwaith A M <131666137+adhwaith43@users.noreply.github.com>
2026-02-02 09:54:43 +0530

    implemented student update functionality

--- StudentApp/students/views.py
+++ StudentApp/students/views.py
@@ -30,7 +30,7 @@ def studentlist(request):
 #primary key based 
 #Api for reading a specific record
 
-@api_view(['GET'])
+@api_view(['GET','PUT'])
 def studentdetail(request,pk):
     #First read the student record from the Student Table
     try:
