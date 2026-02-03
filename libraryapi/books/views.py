2eb749224c7d993f3ef297c8bb89fa71fe5721e2
Adhwaith A M <131666137+adhwaith43@users.noreply.github.com>
2026-02-03 10:28:51 +0530

    added views for book details and list

--- libraryapi/books/views.py
+++ libraryapi/books/views.py
@@ -31,3 +31,31 @@ class BookList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPI
         return self.list(request)
     def post(self,request):
         return self.create(request)
+
+
+# using api views
+from django.http import Http404
+
+class BookDetail(APIView):
+    def get_object(self,pk):
+        try:
+            return Book.objects.get(pk=pk)
+        except:
+            raise Http404
+        
+    def get(self,request,pk):
+        book=self.get_object(pk)
+        b=BookSerializer(book)
+        return Response(b.data,status=status.HTTP_200_OK)
+
+    def put(self,request,pk):
+        book=self.get_object(pk)
+        b=BookSerializer(book,data=request.data)
+        if b.is_valid():
+            b.save()
+            return Response(b.data,status=status.HTTP_200_OK)
+        
+    def delete(self,request,pk):
+        b=self.get_object(pk)
+        b.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
