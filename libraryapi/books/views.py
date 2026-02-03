250cdad824ae0096d91f6649507dc4c68d6f10ec
Adhwaith A M <131666137+adhwaith43@users.noreply.github.com>
2026-02-03 10:41:58 +0530

    update book view to mixins and handle PUT requests

--- libraryapi/books/views.py
+++ libraryapi/books/views.py
@@ -60,3 +60,20 @@ class BookList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPI
 #         b.delete()
 #         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+# mixins
+class BookDetail(mixins.RetrieveModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.DestroyModelMixin,
+                 generics.GenericAPIView):
+    queryset=Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get(self,request,pk):
+        return self.retrieve(request,pk)
+
+    def put(self,request,pk):
+        return self.update(request,pk)
+
+    def delete(self,request,pk):
+        return self.destroy(request,pk)
